b4ba5cc8469dd623b7050a28f09630235d2811c5
ash <ash@umoit.com>
2017-09-13 13:56:43 +0800

    提交信息

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//后台
+Route::group(['prefix' => 'admin'],function(){
+	Route::get('/','Admin\IndexController@index')->name('admin.dashboard');
+	Route::get('login','Admin\LoginController@getLogin')->name('admin.login');
+	Route::post('login','Admin\LoginController@postLogin');
+	Route::get('logout','Admin\LoginController@logout')->name('admin.logout');
+});
